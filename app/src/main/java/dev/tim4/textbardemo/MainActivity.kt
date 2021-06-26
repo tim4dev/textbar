77ab3093bb0c1ea97b4400de34d7011861c08637
tim4dev <developer.tim4dev@gmail.com>
2021-06-26 22:41:38 +0300

    small fix

--- app/src/main/java/dev/tim4/textbardemo/MainActivity.kt
+++ app/src/main/java/dev/tim4/textbardemo/MainActivity.kt
@@ -40,23 +40,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val log = findViewById<TextView>(R.id.textViewLog)
+        val textBarDemo1 = findViewById<TextBar>(R.id.textBarDemo1)
+        val textBarDemo2 = findViewById<TextBar>(R.id.textBarDemo2)
 
-        val texts = listOf(
+        val log1 = findViewById<TextView>(R.id.textViewLog1)
+        val log2 = findViewById<TextView>(R.id.textViewLog2)
+
+        val texts1 = listOf(
             TextRectangleData(text = "S", tag = 111),
             TextRectangleData(text = "L", isChecked = true, tag = 222),
             TextRectangleData(text = "XL", tag = 333),
             TextRectangleData(text = "XXL", tag = 444),
             TextRectangleData(text = "39", tag = 555)
         )
-        val textBarDemo = findViewById<TextBar>(R.id.textBarDemo)
-        textBarDemo.setupTextBar(texts)
+        textBarDemo1.setupTextBar(texts1)
 
         // NOTE. GlobalScope is used here only as an example app.
         GlobalScope.launch(Dispatchers.Main) {
-            textBarDemo.valueFlow.collect { data ->
-                Log.d(TAG, "collect = $data")
-                log.text = "${data.text}, ${data.isChecked}, ${data.tag}"
+            textBarDemo1.valueFlow.collect { data ->
+                Log.d(TAG, "collect1 = $data")
+                log1.text = "${data.text}, ${data.isChecked}, ${data.tag}"
+            }
+        }
+
+        val texts2 = listOf(
+            TextRectangleData(text = "text 1", tag = "tag_1"),
+            TextRectangleData(text = "text 2", isChecked = true, tag = "tag_2"),
+            TextRectangleData(text = "text 3", tag = "tag_3"),
+        )
+        textBarDemo2.setupTextBar(texts2)
+
+        // NOTE. GlobalScope is used here only as an example app.
+        GlobalScope.launch(Dispatchers.Main) {
+            textBarDemo2.valueFlow.collect { data ->
+                Log.d(TAG, "collect2 = $data")
+                log2.text = "${data.text}, ${data.isChecked}, ${data.tag}"
             }
         }
     }
